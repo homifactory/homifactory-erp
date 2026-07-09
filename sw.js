bc8f89218aa5a1fa44b0df81b8a2587b21f65cce
homifactory <hosung@homifactory.com>
2026-07-09 18:27:55 +0900

    휴가: 승인 건 반려로 되돌리기 + 사용 연차 복구 + SW v21

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='homi-erp-v20';
+const CACHE='homi-erp-v21';
 const ASSETS=['/','/index.html','/manifest.json','/icon-192.png','/icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
